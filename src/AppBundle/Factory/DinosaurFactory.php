7802bab29c5e030f35a00e9ddf9fe6ca683065f0
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2018-07-10 08:12:34 +0800

    12. Refactoring & Dependency Injection
    Сделали сервис, в который вынесли часть функционала фабрики. Фабрика сломалась. Изменился конструктор. Хорошо, что в тестах фабрику инициируем в одном месте setUp()

--- src/AppBundle/Factory/DinosaurFactory.php
+++ src/AppBundle/Factory/DinosaurFactory.php
@@ -3,9 +3,20 @@
 namespace AppBundle\Factory;
 
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Service\DinosaurLengthDeterminator;
 
 class DinosaurFactory
 {
+
+    /**
+     * @var DinosaurLengthDeterminator
+     */
+    private $lengthDeterminator;
+
+    public function __construct(DinosaurLengthDeterminator $lengthDeterminator)
+    {
+        $this->lengthDeterminator = $lengthDeterminator;
+    }
     /**
      * @param $length
      * @return Dinosaur
@@ -20,7 +31,7 @@ class DinosaurFactory
         // defaults
         $codeName = 'InG-' . random_int(1, 99999);
         $isCarnivorous = false;
-        $length = $this->getLengthFromSpecification($specification);
+        $length = $this->lengthDeterminator->getLengthFromSpecification($specification);
 
         if (stripos($specification, 'carnivorous') !== false) {
             $isCarnivorous = true;
@@ -43,34 +54,5 @@ class DinosaurFactory
         $dinosaur->setLength($length);
 
         return $dinosaur;
-    }
-
-    /**
-     * @param string $specification
-     * @return int
-     */
-    private function getLengthFromSpecification(string $specification): int
-    {
-        $availableLengths = [
-            'huge' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            'omg' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            '😱' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            'large' => ['min' => Dinosaur::LARGE, 'max' => Dinosaur::HUGE - 1],
-        ];
-        $minLength = 1;
-        $maxLength = Dinosaur::LARGE - 1;
-
-        foreach (explode(' ', $specification) as $keyword) {
-            $keyword = strtolower($keyword);
-
-            if (array_key_exists($keyword, $availableLengths)) {
-                $minLength = $availableLengths[$keyword]['min'];
-                $maxLength = $availableLengths[$keyword]['max'];
-
-                break;
-            }
-        }
-
-        return random_int($minLength, $maxLength);
     }
 }
